f6b2b961bbc5ab94d1a398d0c7101e14af67a5d3
Victor Oliveira <vimatozin@hotmail.com>
2024-11-04 19:04:32 -0300

    added carrocel images

--- src/app/carousel.tsx
+++ src/app/carousel.tsx
@@ -2,35 +2,75 @@
 
 import React from "react";
 import Image from "next/image";
-import { Carousel } from "@material-tailwind/react";
-
-
+import { Carousel, IconButton } from "@material-tailwind/react";
+import car1 from "../../public/image/car1.jpeg";
+import car2 from "../../public/image/car2.jpeg";
+import car3 from "../../public/image/car3.jpeg";
 export function CarouselDefault() {
   return (
     <div className="p-8">
       <div className="w-full container px-5 overflow-hidden rounded-xl relative mx-auto mt-20 max-w-6xl ">        
-        <Carousel className="rounded-xl" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-          <Image
-            width={768}
-            height={400}
-            src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-            alt="image 1"
-            className="h-full w-full object-cover"
-          />
-          <Image
-            width={768}
-            height={400}
-            src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-            alt="image 2"
-            className="h-full w-full object-cover"
-          />
-          <Image
-            width={768}
-            height={400}
-            src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-            alt="image 3"
-            className="h-full w-full object-cover"
-          />
+      <Carousel
+          className="rounded-xl"
+          prevArrow={({ handlePrev }) => (
+            <IconButton
+              variant="text"
+              color="gray"
+              size="lg"
+              onClick={handlePrev}
+              className="!absolute top-2/4 left-4 -translate-y-2/4" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+              </svg>
+            </IconButton>
+          )}
+          nextArrow={({ handleNext }) => (
+            <IconButton
+              variant="text"
+              color="gray"
+              size="lg"
+              onClick={handleNext}
+              className="!absolute top-2/4 !right-4 -translate-y-2/4"  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+              </svg>
+            </IconButton>
+          )} placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}    >
+      <Image
+        src={car1}
+        alt="image 1"
+        className="h-full w-full object-cover"
+      />
+      <Image
+        src={car2}
+        alt="image 2"
+        className="h-full w-full object-cover"
+      />
+      <Image
+        src={car3}
+        alt="image 3"
+        className="h-full w-full object-cover"
+      />
         </Carousel>
       </div>
     </div>
